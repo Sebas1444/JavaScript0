3859b54676baf4d5285ab0554c2fc441ef21487a
Sebas1444 <canhetesebas@gmail.com>
2023-02-21 22:08:08 -0300

    Dia1

---
+++ Dia1/app.js
@@ -0,0 +1,24 @@
+/*
+var a =1;
+var b="Elias";
+
+console.log(a);
+console.warn(a);
+console.error(a);
+console.info(a);
+*/
+/*
+var a="Elias";
+*/
+
+function imprimir(){
+    for (var i = 0; i < 800; i++) {
+        console.log("imprimio");
+    }
+};
+imprimir();
+
+function presionoClik(){
+    console.log("Presiono Boton");
+};
+
